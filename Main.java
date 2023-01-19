cf72d9eee7afb4f6916dc3b8f592574fa07e2368
NilanL <54152496+NilanL@users.noreply.github.com>
2023-01-19 01:23:02 -0500

    Added test suite

--- Main.java
+++ Main.java
@@ -57,8 +57,17 @@ public class Main {
 
         // Get list of 10 largest primes
         var list = runnable.getPrimesList();
+        list.add((long)2);
         Collections.sort(list);
-        topTenList = list.subList(list.size() - 10, list.size());
+
+        if (list.size() > 10)
+        {
+            topTenList = list.subList(list.size() - 10, list.size());
+        }
+        else
+        {
+            topTenList = list;
+        }
     }
 
     // Writes results into primes.txt
@@ -72,7 +81,7 @@ public class Main {
             {
                 file.delete();
             }
-
+            
             file.createNewFile();
 
             FileWriter writer = new FileWriter("primes.txt");
@@ -93,9 +102,80 @@ public class Main {
         }
     }
 
+    public void printResultTest(FileWriter writer, int exp)
+    {
+        try
+        {
+            writer.write("10^" + exp + ": ");
+            writer.write(execTime + "ms " + primesFound + " " + primesTotal);
+            writer.write(" [ ");
+            for (long prime : topTenList)
+            {
+                writer.write(prime + " ");
+            }
+            writer.write("]\n");
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Will test primes from 10^1 to 10^8
+    public void testSuite()
+    {
+        long startSuiteTime = System.nanoTime();
+
+        FileWriter writer = null;
+
+        try
+        {
+            File file = new File("primes_test.txt");
+            
+            if (file.exists())
+            {
+                file.delete();
+            }
+            
+            file.createNewFile();
+
+            writer = new FileWriter("primes_test.txt");
+            
+            for (int i = 1; i <= 8; i++)
+            {
+                Main temp = new Main();
+
+                temp.countPrimes((long)Math.pow(10,i));
+                temp.printResultTest(writer, i);
+            }
+
+            double endSuiteTime = (System.nanoTime() - startSuiteTime) / 1000000.0;
+
+            System.out.println("Testing Suite Done in " + endSuiteTime + " milliseconds");
+            System.out.println("See primes_test.txt");
+
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+            writer = null;
+            System.out.println("ERROR - Suite could not run");
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
         Main assignment1 = new Main();
-        assignment1.countPrimes((long)Math.pow(10, 8));
-        assignment1.printResult();
+
+        if (args == null || args.length == 0 || !args[0].equals("test"))
+        {
+            assignment1.countPrimes((long)Math.pow(10,8));
+            assignment1.printResult();
+        }
+        else
+        {
+            assignment1.testSuite();
+        }
     }
 }
